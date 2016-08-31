db54df2e5826e6561eccd78c3a8623b98319f147
vitaminus <partadw@gmail.com>
2016-08-31 15:52:27 +0300

    Clear controller

--- app/controllers/scrap_controller.rb
+++ app/controllers/scrap_controller.rb
@@ -5,24 +5,17 @@ class ScrapController < ApplicationController
   attr_reader :from, :to, :date
 
   def index
-    # puts params
     @from = params["from"]
     @to = params["to"]
     @date = DateTime.parse(params["date"]).strftime('%F') if params["date"].present? 
-    # puts @date
-    # puts "Works fine!"
     if @from.present? && @to.present? && @date.present?
       octopus = Octopus::United.new(@from,@to,@date)
-      # puts octopus
       @octopus = octopus.get_data
     else
       render 'index'
     end
     respond_to do |format|
-      # if the response fomat is html, redirect as usual
       format.html { }
-
-      # if the response format is javascript, do something else...
       format.js { }
     end
   end
